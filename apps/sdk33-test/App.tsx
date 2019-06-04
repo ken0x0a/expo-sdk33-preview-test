c05c1262220337660a7504ae1aed78ba11d7cb5b
Ken <2770219+ken0x0a@users.noreply.github.com>
2019-06-04 18:34:32 +0900

    mv => apps/app

--- apps/sdk33-test/App.tsx
+++ apps/sdk33-test/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
@@ -13,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
